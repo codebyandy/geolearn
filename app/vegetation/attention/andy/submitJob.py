98fa8b153ef2985bde16348ff96214d6bbba1bcf
codebyandy <andyvhuynh@gmail.com>
2024-05-24 03:09:29 -0700

    uddated submitJob

--- app/vegetation/attention/andy/submitJob.py
+++ app/vegetation/attention/andy/submitJob.py
@@ -51,23 +51,22 @@ def submitJobGPU(jobName, cmdLine, nH=8, nM=16):
     os.system('sbatch {}'.format(jobFile))
 
     
-dropout_lst = [0.2, 0.4, 0.6, 0.8]
-nh_lst = [64, 128, 256]
+dropout_lst = [0.2, 0.4, 0.6]
+# nh_lst = [64, 128,
 
-for dropout in dropout_lst:
-    for nh in nh_lst:
-        run_name = f'no_sampling_do_{dropout}_nh_{nh}'
-        train_path = '/home/users/avhuynh/lfmc/geolearn/app/vegetation/attention/andy/src/train.py'
-        cmd_line = f'python {train_path} --run_name {run_name} --dropout {dropout} --nh {nh} --epochs 1000 --dataset singleDaily-nadgrid --satellites no_landsat'
-        submitJob(run_name, cmd_line)
+# for dropout in dropout_lst:
+#     for nh in nh_lst:
+#         run_name = f'no_sampling_do_{dropout}_nh_{nh}'
+#         train_path = '/home/users/avhuynh/lfmc/geolearn/app/vegetation/attention/andy/src/train.py'
+#         cmd_line = f'python {train_path} --run_name {run_name} --dropout {dropout} --nh {nh} --epochs 1000 --dataset singleDaily-nadgrid --satellites no_landsat'
+#         submitJob(run_name, cmd_line)
 
 
-dropout_lst = [0.2, 0.4, 0.6, 0.8]
-nh_lst = [16, 24, 32, 64, 128, 256]
+nh_lst = [24, 32, 64, 128, 256]
 
 for dropout in dropout_lst:
     for nh in nh_lst:
         run_name = f'no_sampling_do_{dropout}_nh_{nh}'
         train_path = '/home/users/avhuynh/lfmc/geolearn/app/vegetation/attention/andy/src/train_no_sampling.py'
         cmd_line = f'python {train_path} --run_name {run_name} --dropout {dropout} --nh {nh} --epochs 1000 --dataset singleDaily-nadgrid --satellites no_landsat --test_epoch 10'
-        submitJob(run_name, cmd_line, nh=24)
+        submitJob(run_name, cmd_line, nH=24)
