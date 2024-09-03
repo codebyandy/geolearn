fc88db266e7556dd4804b0ed391d314db3fdc8f1
codebyandy <andyvhuynh@gmail.com>
2024-09-03 16:18:48 -0500

    minor mod to prev commit

--- app/vegetation/attention/andy/helpers/checkError.py
+++ app/vegetation/attention/andy/helpers/checkError.py
@@ -5,6 +5,7 @@ files = glob.glob(f"/scratch/users/avhuynh/jobs/*.err")
 newest_err_file = max(files, key=os.path.getmtime)
 
 with open(newest_err_file, 'r') as file:
+    print(newest_err_file)
     contents = file.read()
     print(contents)
 
