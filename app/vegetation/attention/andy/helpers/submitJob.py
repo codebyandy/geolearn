2447be4e4f6126e3c0190889cc9b8d80ae1c2137
codebyandy <andyvhuynh@gmail.com>
2024-09-03 20:25:56 -0500

    minor bugs / prev commit

--- app/vegetation/attention/andy/helpers/submitJob.py
+++ app/vegetation/attention/andy/helpers/submitJob.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     parser.add_argument("--test_epoch", type=int, default=50)
     parser.add_argument("--epochs", type=int, default=500)
     parser.add_argument("--folds", default=DEFAULT_FOLDS)
-    parser.add_argument("--protection", type=bool, default=True)
+    parser.add_argument('--protection', action=argparse.BooleanOptionalAction, default=True)
     args = parser.parse_args()
 
     main(args)
--- app/vegetation/attention/andy/helpers/test.sh
+++ app/vegetation/attention/andy/helpers/test.sh
@@ -1,5 +1,4 @@
-python helpers/submitJob.py --wandb_name debugging --run_name modisgrid-new-const --split_version dataset --dataset singleDaily-modisgrid-new-const --seeds 0 --folds 0,4 --protection False
-python helpers/submitJob.py --wandb_name debugging --run_name modisgrid-new-const --split_version dataset --dataset singleDaily-modisgrid-new-const --seeds 1 --folds 1 --protection False
-python helpers/submitJob.py --wandb_name debugging --run_name modisgrid-new-const --split_version dataset --dataset singleDaily-modisgrid-new-const --seeds 2 --folds 1 --protection False
-
-python helpers/submitJob.py --wandb_name debugging --run_name modisgrid-new-const-strat --split_version stratified --dataset singleDaily-modisgrid-new-const --seeds 2 --folds 3 --protection False
+python helpers/submitJob.py --wandb_name debugging --run_name modisgrid-new-const --split_version dataset --dataset singleDaily-modisgrid-new-const --seeds 0 --folds 0,4 --no-protection
+python helpers/submitJob.py --wandb_name debugging --run_name modisgrid-new-const --split_version dataset --dataset singleDaily-modisgrid-new-const --seeds 1 --folds 1 --no-protection
+python helpers/submitJob.py --wandb_name debugging --run_name modisgrid-new-const --split_version dataset --dataset singleDaily-modisgrid-new-const --seeds 2 --folds 1 --no-protection
+python helpers/submitJob.py --wandb_name debugging --run_name modisgrid-new-const-strat --split_version stratified --dataset singleDaily-modisgrid-new-const --seeds 2 --folds 3 --no-protection
