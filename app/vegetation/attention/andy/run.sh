b65bec8de561caf3f38fe1574def1590a1d8ddc3
codebyandy <andyvhuynh@gmail.com>
2024-09-09 10:19:03 -0700

    testing srun

--- app/vegetation/attention/andy/run.sh
+++ app/vegetation/attention/andy/run.sh
@@ -12,7 +12,7 @@
 # python helpers/submitJob.py --wandb_name debugging --run_name lower_dropout_1seed_strat --split_version stratified --dataset singleDaily-nadgrid --dropout 0.2 --seed 0
 # python helpers/submitJob.py --wandb_name debugging --run_name lower_dropout_1seed_strat --split_version stratified --dataset singleDaily-nadgrid --dropout 0.0 --seed 0
 
-python src/models/cherry_pick/train2.py \
+srun --nodes=1 --time=01:00:00 --job-name=job1 python src/models/cherry_pick/train2.py \
     --wandb_name debugging \
     --run_name debugging2 \
     --seed 0 \
@@ -23,7 +23,7 @@ python src/models/cherry_pick/train2.py \
     --dropout 0.6 \
     --batch_size 500 \
     --epochs 50 
-python src/models/cherry_pick/train.py \
+srun --nodes=1--time=01:00:00 --job-name=job2 python src/models/cherry_pick/train.py \
     --wandb_name debugging \
     --run_name debugging3 \
     --seed 0 \
