c18232f3e5ed673768a238f51f09eaee46d05795
codebyandy <andyvhuynh@gmail.com>
2024-09-22 23:34:02 -0700

    experiments

--- app/vegetation/attention/andy/debug_stratify.sh
+++ app/vegetation/attention/andy/debug_stratify.sh
@@ -1,7 +1,11 @@
-python helpers/submitJob.py --wandb_name default --exp_name stratify --run_name lower_dropouts --dropouts 0,0.2,0.4 
-
-python helpers/submitJob.py --wandb_name default --exp_name stratify --run_name lower_batch_size --batch_sizes
-
-python helpers/submitJob.py --wandb_name default --exp_name stratify --run_name lower_lr --learning_rates
-
-python helpers/submitJob.py --wand_name default --exp_name stratify --run_name optimizer --optimizers adam,sgd
+python helpers/submitJob.py --wandb_name experiments --exp_name stratify --run_name lower_dropouts2 --dropouts 0,0.2,0.4 
+python helpers/submitJob.py --wandb_name experiments --exp_name stratify --run_name lower_batch_size --batch_size
+python helpers/submitJob.py --wandb_name experiments --exp_name stratify --run_name lower_lr --learning_rates
+python helpers/submitJob.py --wand_name experiments --exp_name stratify --run_name optimizer_sgd --optimizers sgd
+python helpers/submitJob.py --wandb_name experiments --exp_name stratify --run_name lower_batch_size_32 --batch_size 32 --iters_per_epoch 200
+python helpers/submitJob.py --wandb_name experiments --exp_name stratify --run_name lower_batch_size_64 --batch_size 64 --iters_per_epoch 100
+python helpers/submitJob.py --wandb_name experiments --exp_name stratify --run_name lower_batch_size_128 --batch_size 128 --iters_per_epoch 50
+python helpers/submitJob.py --wandb_name experiments --exp_name stratify --run_name lower_batch_size_256 --batch_size 256 --iters_per_epoch 25
+python helpers/submitJob.py --wandb_name experiments --exp_name stratify --run_name embedding_sizes --embedding_sizes 32,16
+python helpers/submitJob.py --wandb_name experiments --exp_name stratify --run_name sched_start_epoch_0 --sched_start_epoch 0
+python helpers/submitJob.py --wandb_name experiments --exp_name stratify --run_name sched_start_epoch_100 --sched_start_epoch 100 
